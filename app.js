51a8bc8daadcc44861c176c10d2de42dc39c0922
emmadema <emma.demango@gmail.com>
2017-10-09 15:43:32 -0600

    edit restruant working

--- app.js
+++ app.js
@@ -9,32 +9,70 @@ var url = "mongodb://localhost:27017/restaurant_db";
 //connecting to database
   //sets up collections
 
-  //var allChoice = prompt("Type 'all' and press enter to display all restaurants' names: ");
-  //creating varibale called all chocie
-  //promt method for promt sync database
-  //promt takes repsonses
-var names = [];
 
   mongo.connect(url, function(err, db){
   //connests everthing above and starts function
   var collection = db.collection('restaurants');
-    for (i=0; i<collection.length; i++);
-      i.push(names);
-  var chooseOne = prompt("Type 'name of restruant' and press enter to display name: ");
-    console.log(names[i]);
-    if(names[i] == chooseOne){
-      //if all chocie recives text message of all keep going
-      collection.find().toArray(function(err, doc){
-        //go into collection(restrunats) find everything and spit it out to array
-       console.log(doc);
-        //console logs everything
-      });
-    } else {
-      console.log("Aw, you don't want to see the restaurants?");
-      //type anything else this will fire
-    }
+
+  //var allChoice = prompt("Type 'all' and press enter to display all restaurants' names: ");
+  //creating varibale called all chocie
+  //promt method for promt sync database
+
+//---------- Shows all the Redtruants -----//
+//    var allChoice = prompt("Type 'all' and press enter to display all restaurants' names: ");
+//    if(allChoice == "all"){
+//        collection.find().toArray(function(err, doc){
+//        console.log(doc);
+//     });
+//  } else {
+//    console.log("Aw, you don't want to see the restaurants?");
+//  }
+//});
+
+// ---- Shows one resturant only when typed in ------ //
+
+//  var chooseOne = prompt("Type the name of the restaurant and press enter to display the restaurant's info: ");
+//  collection.find({ "name" : chooseOne }).forEach( function(doc){
+//    console.log(doc);
+//  });
+
+// ------ Users add opne resturant -----//
+
+  var addOne = prompt("Type 'yes' to add a new resturant: ");
+  //promts the use to start the add one proccess
+
+  var addOneName = prompt("Type the name of the resturnat and press enter to add the new name: ");
+  // add the resturnats name
+
+  var addOneStreet = prompt("Type the address of the resturant and press enter to add the address: ");
+  // adds the restruants street address
+
+  var addOneCity = prompt("Type the city the resturant is loacted in and press enter to add the city: ");
+  // adds the resturants city
+
+  var addOneYelp = prompt("Type the yelp URL and press enter to add the URL: ");
+  //adds the restrunrants yelp address
+
+  var newRestaurant = { "name" : addOneName, "street" : addOneStreet, "city" : addOneCity, "yelp" : addOneYelp }
+  //adds each item above into a varibale new restruant 
+
+  if (addOne == "yes") {
+    //id the add one promt is yes
+
+    collection.insert(newRestaurant);
+    //insert new restruant into the restruant database
+
+  } else {
+    console.log("Aw, you don't want to add a restaurant?");
+    //otherwise console log statemnt
+  };
+  console.log(newRestaurant);
+  //console log tthe new restruant that was added
+
+
+// ------ Users edit a resturant -------//
+
+
+//--------- Users delete restruant --------//
+
 });
-//Task 1
-//add a promt that allows you to use .find and for each to 
-//just find something for a specific name
- 
